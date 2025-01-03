85383bd850f973c03a72ccaf6f157e0f568c9c3c
kaitotamura <73684714+kaitotamura@users.noreply.github.com>
2025-01-03 17:13:51 +0900

    Update main.go

--- go/excellent/main.go
+++ go/excellent/main.go
@@ -1,9 +1,12 @@
 package main
 
+// EvenOrOdd checks if a number is even or odd.
 func EvenOrOdd(number int) string {
+	// If the number is divisible by 2, it's even.
 	if number%2 == 0 {
 		return "even"
 	} else {
+		// Otherwise, it's odd.
 		return "odd"
 	}
 }
